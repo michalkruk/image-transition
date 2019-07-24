6fec25e461430527a58be2d5ceb0a7c071a8a274
michalkruk <michal.k.kruk@gmail.com>
2019-07-24 20:16:24 +0200

    up

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./photos/cat1.png",
+    image2: "./photos/cat2.png",
+    displacementImage: "./photos/strpies.png"
+});
